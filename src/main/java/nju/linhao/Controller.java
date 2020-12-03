7c1291fa080945e488b705834cfc706bac614bb0
LaJiOJ <1184264181@qq.com>
2020-12-03 15:25:43 +0800

    Reorganize files

--- src/main/java/nju/linhao/Controller.java
+++ src/main/java/nju/linhao/Controller.java
@@ -1,4 +1,61 @@
 package main.java.nju.linhao;
 
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
 public class Controller {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="mainCanvas"
+    private Canvas mainCanvas; // Value injected by FXMLLoader
+
+    @FXML // fx:id="startButton"
+    private Button startButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="loadButton"
+    private Button loadButton; // Value injected by FXMLLoader
+
+    @FXML
+    void startButtonClicked(MouseEvent event) {
+        System.out.println("开始游戏！");
+    }
+
+    @FXML
+    void loadButtonClicked(MouseEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("打开战斗记录");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+        fileChooser.showOpenDialog(null);
+    }
+
+    
+    // Codes below are for init
+    private static final int CANVAS_WIDTH = 400;
+    private static final int CANVAS_HEIGHT = 300;
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert mainCanvas != null : "fx:id=\"mainCanvas\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert startButton != null : "fx:id=\"startButton\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert loadButton != null : "fx:id=\"loadButton\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        final Image img = new Image(getClass().getResourceAsStream("/pictures/background.jpg"));
+        GraphicsContext graphicsContext = mainCanvas.getGraphicsContext2D();
+        graphicsContext.save();
+        graphicsContext.drawImage(img, 0, 0,480,360);
+        graphicsContext.restore();
+    }
 }
--- src/main/java/nju/linhao/Main.java
+++ src/main/java/nju/linhao/Main.java
@@ -19,17 +19,18 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 public class Main extends Application {
-    private static final int CANVAS_WIDTH = 400;
-    private static final int CANVAS_HEIGHT = 300;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Class<?> curClass = this.getClass();
-        Scene scene = WindowInit(primaryStage, curClass);
+        Parent parent = FXMLLoader.load(curClass.getResource("/MainWindow.fxml"));
+        Scene scene = new Scene(parent);
+
         primaryStage.setTitle("CalabashKids VS Monsters");
         primaryStage.getIcons()
                 .add(new Image(curClass.getResourceAsStream("/icon/CalabashKidsVSMonstersIcon.png")
@@ -43,27 +44,4 @@ public class Main extends Application {
         launch(args);
     }
 
-    private Scene WindowInit(Stage primaryStage, Class<?> curClass) throws Exception{
-        VBox root = FXMLLoader.load(curClass.getResource("/MainWindow.fxml"));
-
-        final Image img = new Image(curClass.getResourceAsStream("/pictures/background.jpg"));
-        final Canvas canvas = new Canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-        root.getChildren().add(canvas);
-        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
-        graphicsContext.save();
-        graphicsContext.drawImage(img, 0, 0);
-        graphicsContext.restore();
-
-        Button startButton = new Button("开始战斗");
-        Button openButton = new Button("回放记录");
-
-        HBox hBox = new HBox(50);
-        hBox.setPadding(new Insets(0, 100, 0, 100));
-        hBox.getChildren().add(startButton);
-        hBox.getChildren().add(openButton);
-        root.getChildren().add(hBox);
-
-        Scene scene = new Scene(root);
-        return scene;
-    }
 }
