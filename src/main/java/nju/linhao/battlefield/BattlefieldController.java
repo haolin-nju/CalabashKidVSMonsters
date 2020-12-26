c4e95182dda008c7b5be5bdd97bfede799b66a04
LaJiOJ <1184264181@qq.com>
2020-12-26 23:45:35 +0800

    Update

--- src/main/java/nju/linhao/battlefield/BattlefieldController.java
+++ src/main/java/nju/linhao/battlefield/BattlefieldController.java
@@ -117,7 +117,6 @@ public class BattlefieldController {
                 if(bullet!=null){
                     battlefield.addBullet(bullet);
                 }
-
             }
         }
         repaint();
